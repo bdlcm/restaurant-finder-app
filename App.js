a3be1a19cb41b358b532ebda123d8a943f544778
barbara <bmoah001@gmail.com>
2021-04-18 12:39:12 +0200

    added tabs

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import {Text} from 'react-native';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { theme } from './src/infrastructure/theme';
  import { ThemeProvider } from 'styled-components';
+ import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
  import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -13,6 +18,10 @@ import {
 } from '@expo-google-fonts/lato';
  
 const isAndroid = Platform.OS === 'android';
+
+const Tab = createBottomTabNavigator();
+const Settings = () =><Text>Settings</Text>
+const Map = () =><Text>Map</Text>
 
 
 export default function App() {
@@ -29,7 +38,14 @@ export default function App() {
    return (
     <>
     <ThemeProvider theme={theme}>
-    <RestaurantsScreen />
+   <NavigationContainer>
+   <Tab.Navigator>
+    <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+    <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Screen name="Map" component={Map} />
+
+  </Tab.Navigator>
+   </NavigationContainer>
 
     </ThemeProvider>
       <ExpoStatusBar style="auto" />
