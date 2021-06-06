531e901b6d64591c2c180d2f5b1c7117045073a3
barbara <bmoah001@gmail.com>
2021-06-06 13:59:35 +0200

    finsihed integrating apids

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,23 @@
 const { locations: locationsMock } = require("./geocode.mock");
-const URL = require("url").URL;
+ const functions = require('firebase-functions');
 
-module.exports.geocodeRequest = (request, response) => {
-    const { city } = request.query;
-    const locationMock= locationsMock[city.toLowerCase()];
-   response.send(locationMock);
+module.exports.geocodeRequest = (request, response, client) => {
+    const { city, mock } = request.query;
+    if (mock === "true"){
+        const locationMock= locationsMock[city.toLowerCase()];
+        return  response.send(locationMock);
+    }
+  client.geocode({
+      params: {
+          address: city,
+          key: functions.config().google.key,
+      },
+
+      timeout:1000,
+  }).then((result) => {
+      return response.json(result.data)
+  }).catch((err) => {
+      response.status(400);
+     return response.send(err.response.data.error_message)
+   });
 };
--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,19 @@ const functions = require("firebase-functions");
 const { geocodeRequest } = require("./geocode");
 const { placesRequest } = require("./places");
 
+
+const {Client} = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
  
  exports.geocode = functions.https.onRequest((request, response) => {
-  geocodeRequest(request, response);
+  geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-    placesRequest(request, response);
+    placesRequest(request, response, client);
   });
