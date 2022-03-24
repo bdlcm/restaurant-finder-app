03a972a9ac005c4801ea2c122913575669f76eeb
barbara <barbara@bmoah001@gmail.com>
2022-03-24 14:45:49 +0200

    minor change

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -2,9 +2,7 @@ import camelize from "camelize";
 import { host } from "../../utils/env";
 
 export const restaurantsRequest = (location) => {
-  return fetch(
-    `${host}/placesNearby?location=${location}`
-  ).then((res) => {
+  return fetch(`${host}/placesNearby?location=${location}`).then((res) => {
     return res.json();
   });
 };
